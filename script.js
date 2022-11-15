2a9dd41f08c39c6424f8f46c72c8ea982c27e329
Okafor Patrick Chibuike <Patrickpeters911@gmail.com>
2022-11-15 18:11:41 +0100

    add dynamics

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-const spin = document.getElementById('spinner')
-const btn = document.querySelector('button');
-const inputValue = document.getElementById('input').value;
+let btn = document.getElementById('btn');
+let spin = document.getElementById('spinner');
+let errorMsg =  document.getElementById('error-msg');
+let qrImage = document.getElementById('qr-image');
+console.log(qrImage);
 
-btn.addEventListener('click', showSpinner);
+const clickButton = () =>{
 
-function showSpinner(){
-    if (inputValue === '') {
-        alert('erro')
-        
-    }else{
-        showSpinner()
+    let inputValue = document.getElementById('url').value;
+    
+    if (!inputValue) {
+    errorMsg.style.display = 'block';
+    qrImage.style.display= 'none';
+    btn.innerText = 'Generate Code';
 
-        setInterval(() => {
-            hideSpinner()
-        }, 1000);
+   
     }
+    else {
+    showSpinner();
+    errorMsg.style.display = 'none';
+    qrImage.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+    qrImage.style.display = 'block'
+
+    }
+    setInterval(() => stopSpinner() , 222);
+
 }
 
+qrImage.addEventListener('load', generateText)
 
-function showSpinner(){
-    spin.style.display = 'inline-block' 
+ function generateText (){
+    btn.innerText = 'Generating Code...';
 }
 
+btn.addEventListener('click', clickButton);
 
-function hideSpinner(){
-
-    spin.style.display = 'none'  
-        
+function stopSpinner() {
+    spin.style.display = 'none'
 }
-
-
+function showSpinner() {
+    spin.style.display = 'inline-block'
+}
  
